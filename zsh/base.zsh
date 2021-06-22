ab76cd52aea5e1ce152db0cb25509efc29bd7138
imtyM <m.imtiazmukadam@gmail.com>
2021-06-22 21:30:04 +0200

    Update

--- zsh/base.zsh
+++ zsh/base.zsh
@@ -1,5 +1,7 @@
 stty -ixon
 
+. $HOME/.asdf/asdf.sh
+
 export EDITOR='vim'
 export APPS_HOME=/home/imtiaz/apps
 export ANDROID_HOME=/home/imtiaz/Android
@@ -7,3 +9,11 @@ export ANDROID_SDK_ROOT=/home/imtiaz/Android
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
 
 export PATH="$PATH:$JAVA_HOME/bin:$ANDROID_HOME/emulator:$ANDROID_HOME/tools:$ANDROID_HOME/tools/bin:$ANDROID_HOME/platform-tools:$APPS_HOME"
+
+TIMEFMT='%J   %U  user %S system %P cpu %*E total'$'\n'\
+'avg shared (code):         %X KB'$'\n'\
+'avg unshared (data/stack): %D KB'$'\n'\
+'total (sum):               %K KB'$'\n'\
+'max memory:                %M MB'$'\n'\
+'page faults from disk:     %F'$'\n'\
+'other page faults:         %R'
