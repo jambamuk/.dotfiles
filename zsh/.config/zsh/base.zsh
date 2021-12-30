4626df9ab47c91ce7009e5da70c3bb7620e846e3
imtyM <m.imtiazmukadam@gmail.com>
2021-12-30 17:10:47 +0200

    squash me later

--- zsh/.config/zsh/base.zsh
+++ zsh/.config/zsh/base.zsh
@@ -7,6 +7,7 @@ export APPS_HOME=/home/imtiaz/apps
 export ANDROID_HOME=/home/imtiaz/Android
 export ANDROID_SDK_ROOT=/home/imtiaz/Android
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+alias ranger='ranger --choosedir=$HOME/.rangerdir; LASTDIR=`cat $HOME/.rangerdir`; cd "$LASTDIR"'
 
 export PATH="$PATH:$JAVA_HOME/bin:$ANDROID_HOME/emulator:$ANDROID_HOME/tools:$ANDROID_HOME/tools/bin:$ANDROID_HOME/platform-tools:$APPS_HOME"
 
